cfe58221dfe806fe872225839b43f2e18bdbca4c
Jakob Houser <jakob.houser@gmail.com>
2024-08-14 10:47:17 -0700

    Add user and password spec for registration

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/jhouser/actionphase/backend/pkg/http v0.0.0-00010101000000-000000000000
+	golang.org/x/crypto v0.20.0
 )
 
 require (
@@ -19,7 +20,6 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
---
+++ backend/pkg/actionphase/users.go
@@ -0,0 +1,41 @@
+package actionphase
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+type User struct {
+	ID        int        `json:"id"`
+	Name      string     `json:"name"`
+	Email     string     `json:"email"`
+	Password  Password   `json:"-"`
+	CreatedAt *time.Time `json:"createdAt"`
+}
+
+type Password struct {
+	Plaintext *string
+	Hash      string
+}
+
+func (p *Password) GeneratePasswordHash(plaintext string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(plaintext), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	p.Plaintext = &plaintext
+	p.Hash = string(bytes)
+	return nil
+}
+
+func (p *Password) CheckPasswordHash(plaintext string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(p.Hash), []byte(plaintext))
+	return err == nil
+}
+
+type UserService interface {
+	User(id int) (*User, error)
+	Users() ([]*User, error)
+	CreateUser(u *User) error
+	DeleteUser(id int) error
+}
