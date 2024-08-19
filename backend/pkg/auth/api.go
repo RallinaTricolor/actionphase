0ebbe0c12dc87950009ad14865cf549a9bebc70d
Rallina Tricolor <rallina.circumflex@gmail.com>
2024-08-19 13:58:49 -0700

    Refactor auth file structure

---
+++ backend/pkg/auth/api.go
@@ -0,0 +1,31 @@
+package auth
+
+import (
+	"actionphase/pkg/core"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"net/http"
+)
+
+type Request struct {
+	*core.User
+}
+
+func (r *Request) Bind(req *http.Request) error {
+	if r.User == nil {
+		return fmt.Errorf("missing required User fields")
+	}
+	return nil
+}
+
+type Handler struct {
+	DB *pgxpool.Pool
+}
+
+type Response struct {
+	*core.User
+}
+
+func (rd *Response) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
