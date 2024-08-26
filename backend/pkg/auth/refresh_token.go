90007f6e7e02f5f3af3d47431eabe4c62e452fa1
Rallina Tricolor <rallina.circumflex@gmail.com>
2024-08-26 07:47:17 -0700

    Ensure token exists in DB while verifying it

--- backend/pkg/auth/refresh_token.go
+++ backend/pkg/auth/refresh_token.go
@@ -10,17 +10,20 @@ func (h *Handler) V1Refresh(w http.ResponseWriter, r *http.Request) {
 	tokenString := r.Header.Get("Authorization")[len("Bearer "):]
 	jwt := JWTHandler{App: h.App}
 	if err := jwt.VerifyToken(tokenString); err != nil {
+		h.App.Logger.Error("Error verifying token", "error", err)
 		render.Render(w, r, core.ErrInvalidRequest(err))
 		return
 	}
 	tokenClaims, err := jwt.DecodeToken(tokenString)
 	if err != nil {
+		h.App.Logger.Error("Error decoding token", "error", err)
 		render.Render(w, r, core.ErrInvalidRequest(err))
 		return
 	}
 	h.App.Logger.Info("Creating refreshed token for user", "username", tokenClaims["username"].(string))
 	token, err := jwt.CreateToken(tokenClaims["username"].(string))
 	if err != nil {
+		h.App.Logger.Error("Error creating token", "error", err)
 		render.Render(w, r, core.ErrInternalError(err))
 		return
 	}
